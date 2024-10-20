c68f7e0879ed6782c9b28e35a88b004640483e35
toitenanhtuan <nguyenanhtuan3052004@gmail.com>
2024-10-21 00:28:12 +0700

    update image

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,17 @@
+class ImageConstant {
+  static String imagePath = 'assets/images';
+
+  // TrangCF
+  static String imgCappuccino_lowmilk = '$imagePath/cappuccino_lowmilk.png';
+
+  // thanh toan
+  static String imgImage1 = '$imagePath/image_1.png';
+  static String imgArrowRight = '$imagePath/arrow_right.svg';
+  static String imgCappuccinoSmall46x52 = '$imagePath/cappuccino_small_46x52.png';
+  static String imgNoteText = '$imagePath/note_text_1.svg';
+  static String imgThumbsUp = '$imagePath/thumbs_up.svg';
+  static String imgNavHomeBlueGray = '$imagePath/home_blue_gray.svg';
+  static String imgNavCartPrimary = '$imagePath/cart_primary.svg';
+
+  // tra sua
+}
