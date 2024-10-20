ee6ce6f7a93646294d6fa531c3a41e830fe6f973
toitenanhtuan <nguyenanhtuan3052004@gmail.com>
2024-10-21 01:49:00 +0700

    update utils

--- lib/core/utils/image_constant.dart
+++ lib/core/utils/image_constant.dart
@@ -14,4 +14,36 @@ class ImageConstant {
   static String imgNavCartPrimary = '$imagePath/cart_primary.svg';
 
   // tra sua
+  static String imgTraSuaSmall = '$imagePath/trasua_small.png';
+  static String imgThaiXanhSmall = '$imagePath/thaixanh_small.png';
+  static String imgTraDau = '$imagePath/tradau.png';
+
+  // sinh to
+  static String imgSinhToBoSmall = '$imagePath/sinhtobo_small.png';
+  static String imgSinhToXoaiSmall = '$imagePath/sinhtoxoai_small.png';
+  static String imgSinhToChuoi = '$imagePath/sinhtochuoi.png';
+
+  // Common
+  static String imgGetStarted = '$imagePath/main_get_star.png';
+  static String imgCappuccinoFall = '$imagePath/cappuccino_fall.png';
+  static String imgArrowLeft = '$imagePath/arrow_left.svg';
+  static String imgSignal = '$imagePath/signal.svg';
+  static String imgOpenmojiRoastedCoffeeBean = '$imagePath/openmoji_roasted_coffee_bean.svg';
+  static String imgTelevision = '$imagePath/television.svg';
+  static String imgLock = '$imagePath/lock.svg';
+  static String imgCarbonLocationFilled = '$imagePath/carbon_location_filled.svg';
+  static String imgNotification = '$imagePath/notification.svg';
+  static String imgSearch = '$imagePath/search.svg';
+  static String imgIconparksettingconfig = '$imagePath/park_setting_config.svg';
+  static String imgCappuccino84x110 = '$imagePath/cappuccino_fall.png';
+  static String imgCappuccino98x128 = '$imagePath/cappuccino_lowmilk.png';
+  static String imgPlus = '$imagePath/plus.svg';
+  static String imgFireEmote = '$imagePath/fire_emote.svg';
+  static String imgCappuccino106x138 = '$imagePath/cappuccino_small_106x138.png';
+  static String imgNavHome = '$imagePath/home.svg';
+  static String imgNavFavourite = '$imagePath/favourite.svg';
+  static String imgNavCart= '$imagePath/cart.svg';
+  static String imgNavProfile = '$imagePath/profile.svg';
+  static String imgTraSua350x374 = '$imagePath/trasua_fall.png';
+  static String imgSinhToBo346x374 = '$imagePath/sinhtobo_fall.png';
 }
