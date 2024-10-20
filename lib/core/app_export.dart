a0ccc04a77846d536f1a7a0b1a1aa8d5fbb5b1cc
toitenanhtuan <nguyenanhtuan3052004@gmail.com>
2024-10-20 23:29:00 +0700

    update assets

---
+++ lib/core/app_export.dart
@@ -0,0 +1,3 @@
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export '';
---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:appqlcafe/main.dart';
+
+abstract class NetworkInfoI{
+  Future<bool> isConnected();
+
+  Future<List<ConnectivityResult>> get connectivityResult;
+
+  Stream<List<ConnectivityResult>> get onConnectivityChanged;
+}
+
+class NetworkInfo implements NetworkInfoI{
+  Connectivity connectivity;
+
+  static final NetworkInfo _networkInfo = NetworkInfo._internal(Connectivity());
+
+  factory NetworkInfo(){
+    return _networkInfo;
+  }
+
+  NetworkInfo._internal(this.connectivity){
+    connectivity = this.connectivity;
+  }
+
+  @override
+  Future<bool> isConnected() async {
+    final result = await connectivityResult;
+    return !result.contains(ConnectivityResult.none);
+  }
+
+  @override
+  Future<List<ConnectivityResult>> get connectivityResult async{
+    return connectivity.checkConnectivity();
+  }
+
+  @override
+  Stream<List<ConnectivityResult>> get onConnectivityChanged =>
+      connectivity.onConnectivityChanged;
+}
+
+abstract class Failure {}
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
+
+class NetworkFailure extends Failure {}
+
+class ServerException implements Exception {}
+
+class CacheException implements Exception {}
+
+class NetworkException implements Exception {}
+
+class NoInternetException implements Exception {
+  late String _message;
+
+  NoInternetException([String message = 'NoInternetException Occurred']) {
+    if (globalMessengerKey.currentState != null) {
+      globalMessengerKey.currentState!
+          .showSnackBar(SnackBar(content: Text(message)));
+    }
+    this._message = message;
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return _message;
+  }
+}
